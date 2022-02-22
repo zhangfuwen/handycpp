e8135dd2730d3c841d399e2c2294fe7bcff77d9b
zhangfuwen <zhangfuwen@foxmail.com>
2022-02-22 11:47:55 +0800

    print error if failed to open file

--- include/handycpp/file.h
+++ include/handycpp/file.h
@@ -8,8 +8,29 @@
 #include <string>
 #include <fstream>
 #include <cstring>
+#include <dirent.h>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <functional>
+
 #include "handycpp/logging.h"
+
 namespace handycpp::file {
+
+static inline int removeFile(std::string file) {
+    return remove(file.c_str());
+}
+
+static inline int renameFile(std::string old, std::string newfile) {
+    return std::rename(old.c_str(), newfile.c_str());
+}
+
+static inline int removeDir(std::string dir) {
+    return rmdir(dir.c_str());
+}
+
+
 static inline auto getFileSize(const std::string& path)
 {
     FILE *f;
@@ -49,9 +70,51 @@ static inline bool saveFile(char *data, int size, const std::string& filename = 
     FUN_DEBUG("filepath %s", filename.c_str());
     std::ofstream myfile;
     myfile.open (filename);
+    if(!myfile.is_open()) {
+        FUN_ERROR("failed to open file %s", filename.c_str());
+        return false;
+    }
     myfile.write(data, size);
     myfile.close();
     return true;
+}
+
+
+void listFiles(const std::string& path, std::vector<std::string> &files, bool recursive = false, const bool showHiddenDirs = false, bool include_folders=false){
+    DIR *dpdf;
+    struct dirent *epdf;
+    dpdf = opendir(path.c_str());
+    if (dpdf != NULL){
+        while ((epdf = readdir(dpdf)) != NULL){
+            if(showHiddenDirs ? (epdf->d_type==DT_DIR && std::string(epdf->d_name) != ".." && std::string(epdf->d_name) != "." ) : (epdf->d_type==DT_DIR && strstr(epdf->d_name,"..") == NULL && strstr(epdf->d_name,".") == NULL ) ){
+                if(recursive) {
+                    listFiles(path+epdf->d_name+"/",files, showHiddenDirs);
+                }
+            }
+            if(epdf->d_type == DT_DIR ) {
+                if(include_folders) {
+                    files.push_back(path+epdf->d_name);
+                }
+            } else {
+                files.push_back(path+epdf->d_name);
+            }
+        }
+    }
+    closedir(dpdf);
+}
+
+void listFiles(const std::string &path, std::function<void(const std::string &)> cb) {
+    if (auto dir = opendir(path.c_str())) {
+        while (auto f = readdir(dir)) {
+            if (!f->d_name || f->d_name[0] == '.') continue;
+            if (f->d_type == DT_DIR)  {
+                listFiles(path + f->d_name + "/", cb);
+            } else {
+                cb(path + f->d_name);
+            }
+        }
+        closedir(dir);
+    }
 }
 
 }
