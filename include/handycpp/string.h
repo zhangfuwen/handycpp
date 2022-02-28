2c6fab44dec87d3f61014df83cf657f820839ac9
zhangfuwen <zhangfuwen@foxmail.com>
2022-02-28 18:46:08 +0800

    add starts&ends_with

--- include/handycpp/string.h
+++ include/handycpp/string.h
@@ -7,46 +7,60 @@
 
 #include <string>
 #include <algorithm>
+namespace handycpp::string {
 
 // trim from start (in place)
-static inline std::string& ltrim(std::string &s) {
-  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-            return !std::isspace(ch);
-          }));
-  return s;
+static inline std::string &ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) { return !std::isspace(ch); }));
+    return s;
 }
 
 // trim from end (in place)
-static inline std::string& rtrim(std::string &s) {
-  s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-            return !std::isspace(ch);
-          }).base(), s.end());
-  return s;
+static inline std::string &rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !std::isspace(ch); }).base(), s.end());
+    return s;
 }
 
 // trim from both ends (in place)
-static inline std::string& trim(std::string &s) {
-  ltrim(s);
-  rtrim(s);
-  return s;
+static inline std::string &trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+    return s;
 }
 
 // trim from start (copying)
 static inline std::string ltrim_copy(std::string s) {
-  ltrim(s);
-  return s;
+    ltrim(s);
+    return s;
 }
 
 // trim from end (copying)
 static inline std::string rtrim_copy(std::string s) {
-  rtrim(s);
-  return s;
+    rtrim(s);
+    return s;
 }
 
 // trim from both ends (copying)
 static inline std::string trim_copy(std::string s) {
-  trim(s);
-  return s;
+    trim(s);
+    return s;
 }
+
+#if __cplusplus >= 202002L
+static inline bool starts_with(std::string_view str, std::string_view prefix) {
+    return str.starts_with(prefix);
+}
+static inline bool ends_with(std::string_view str, std::string_view suffix) {
+    return str.ends_with(suffix);
+}
+#else
+static inline bool starts_with(const std::string &str, const std::string &prefix) {
+    return str.substr(0, prefix.size()) == prefix;
+}
+static inline bool ends_with(const std::string &str, const std::string &suffix) {
+    return str.substr(str.size() - suffix.size()) == suffix;
+}
+#endif
+};
 
 #endif // HANDYCPP_STRING_H
--- test/test.cpp
+++ test/test.cpp
@@ -2,6 +2,7 @@
 // Created by zhangfuwen on 2022/2/21.
 //
 
+#include <cassert>
 #include "handycpp/file.h"
 #include "handycpp/logging.h"
 #include "handycpp/string.h"
@@ -16,5 +17,11 @@ int main() {
     } else {
         printf("failed to read");
     }
+    printf("%lu\n", __cplusplus);
+    std::string s = "hello world";
+    assert(1 == handycpp::string::starts_with(s, "hello"));
+    assert(1 == handycpp::string::ends_with(s, "world"));
+    assert(0 == handycpp::string::starts_with(s, "ello"));
+    assert(0 == handycpp::string::ends_with(s, "worl"));
     return 0;
 }
