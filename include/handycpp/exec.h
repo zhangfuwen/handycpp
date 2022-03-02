c665800caaca7b6ac8d6ae1407bfafbdc6dfe090
zhangfuwen <zhangfuwen@foxmail.com>
2022-03-02 11:45:45 +0800

    fix missing headers

--- include/handycpp/exec.h
+++ include/handycpp/exec.h
@@ -6,9 +6,11 @@
 #define HANDYCPP_EXEC_H
 #include <string>
 #include <array>
+#include <memory>
+#include <cstdio>
 
 static inline std::string exec(const char *cmd) {
-  std::array<char, 128> buffer;
+  std::array<char, 128> buffer{};
   std::string result;
   std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
   if (!pipe) {
