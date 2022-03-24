88bb5f2be66ab63fbe106bb0d7ff1ff5bfce8786
zhangfuwen <zhangfuwen@bytedance.com>
2022-03-24 14:26:30 +0800

    format file.h

--- include/handycpp/file.h
+++ include/handycpp/file.h
@@ -10,9 +10,10 @@
 #include <fstream>
 #include <functional>
 #include <libgen.h>
-#include <string>
-#include <vector>
 #include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
 
 #include "handycpp/logging.h"
 #if __cplusplus >= 201703L
@@ -23,11 +24,13 @@
 
 namespace handycpp::file {
 
-static inline int removeFile(std::string file) { return remove(file.c_str()); }
+static inline int removeFile(const std::string &file) { return remove(file.c_str()); }
 
-static inline int renameFile(std::string old, std::string newfile) { return std::rename(old.c_str(), newfile.c_str()); }
+static inline int renameFile(const std::string &old, const std::string &newfile) {
+    return std::rename(old.c_str(), newfile.c_str());
+}
 
-static inline int removeDir(std::string dir) { return rmdir(dir.c_str()); }
+static inline int removeDir(const std::string &dir) { return rmdir(dir.c_str()); }
 
 static inline size_t getFileSize(const std::string &path) {
     FILE *f;
@@ -59,22 +62,22 @@ struct mem_chunk {
  *      return -1 on error, and errno is set
  *      return number of lines parsed
  */
-    static inline int for_each_line(std::string filePath, std::function<int(int, std::string)> lineOp) {
-        std::ifstream input(filePath);
-        if (!input.good()) {
-            errno = ENOENT;
-            return -1;
-        }
-        int i = 0;
-        for (std::string line; getline(input, line);) {
-            if(auto ret = lineOp(i, line); ret < 0) {
-                i++;
-                return i;
-            }
-            i++;
-        }
-        return i;
+static inline int for_each_line(const std::string &filePath, const std::function<int(int, std::string)> &lineOp) {
+    std::ifstream input(filePath);
+    if (!input.good()) {
+        errno = ENOENT;
+        return -1;
     }
+    int i = 0;
+    for (std::string line; getline(input, line);) {
+        if (auto ret = lineOp(i, line); ret < 0) {
+            i++;
+            return i;
+        }
+        i++;
+    }
+    return i;
+}
 
 static inline bool is_file_exist(const char *fileName) {
     std::ifstream infile(fileName);
@@ -82,7 +85,6 @@ static inline bool is_file_exist(const char *fileName) {
 }
 
 #if __cplusplus >= 201703L && !defined(ANDROID)
-
 #else
 #include <sys/stat.h>
 #endif
@@ -93,11 +95,14 @@ static inline bool is_dir_exist(const char *dirname) {
 #else
     struct stat info;
 
-    int statRC = stat( dirname, &info );
-    if( statRC != 0 )
-    {
-        if (errno == ENOENT)  { return 0; } // something along the path does not exist
-        if (errno == ENOTDIR) { return 0; } // something in path prefix is not a dir
+    int statRC = stat(dirname, &info);
+    if (statRC != 0) {
+        if (errno == ENOENT) {
+            return 0;
+        } // something along the path does not exist
+        if (errno == ENOTDIR) {
+            return 0;
+        } // something in path prefix is not a dir
         return -1;
     }
 
@@ -105,17 +110,16 @@ static inline bool is_dir_exist(const char *dirname) {
 #endif
 }
 
-#include <string>
 #include <fstream>
 #include <streambuf>
-static inline std::string readText(std::string path) {
+#include <string>
+static inline std::string readText(const std::string &path) {
     std::ifstream t(path);
-    std::string str((std::istreambuf_iterator<char>(t)),
-                    std::istreambuf_iterator<char>());
+    std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
     return str;
 }
 
-static inline mem_chunk readFile(std::string path, size_t size = 0) {
+static inline mem_chunk readFile(const std::string &path, size_t size = 0) {
     if (size == 0) {
         size = getFileSize(path);
     }
@@ -129,15 +133,15 @@ static inline mem_chunk readFile(std::string path, size_t size = 0) {
 
     std::ifstream myfile;
     myfile.open(path);
-    myfile.read((char *)ret.addr, ret.size);
+    myfile.read((char *)ret.addr, (int)ret.size);
     myfile.close();
 
     return ret;
 }
 
-static inline std::string readTextFile(std::string path) {
-    std::ostringstream  stream;
-    int ret = for_each_line(path, [&stream](int n, std::string line) {
+static inline std::string readTextFile(const std::string& path) {
+    std::ostringstream stream;
+    int ret = for_each_line(path, [&stream](int n, const std::string &line) {
         (void)n;
         stream << line;
         return 0;
@@ -149,12 +153,12 @@ static inline std::string readTextFile(std::string path) {
 }
 
 #if defined(ANDROID)
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 #endif
 
-inline static int create_dir(const char *path, [[maybe_unused]] bool recursive = false) {
+inline static int create_dir([[maybe_unused]] const char *path, [[maybe_unused]] bool recursive = false) {
 #if __cplusplus >= 201703L && !defined(ANDROID)
     std::error_code ec;
     std::filesystem::create_directories(path, ec);
@@ -164,7 +168,7 @@ inline static int create_dir(const char *path, [[maybe_unused]] bool recursive =
     struct stat st = {0};
 
     if (stat("/some/directory", &st) == -1) {
-       mkdir("/some/directory", 0700);
+        mkdir("/some/directory", 0700);
     }
     return -1; // not implemented, :)
 #endif
@@ -202,12 +206,12 @@ static inline void listFiles(
     DIR *dpdf;
     struct dirent *epdf;
     dpdf = opendir(path.c_str());
-    if (dpdf != NULL) {
-        while ((epdf = readdir(dpdf)) != NULL) {
+    if (dpdf != nullptr) {
+        while ((epdf = readdir(dpdf)) != nullptr) {
             if (showHiddenDirs
                     ? (epdf->d_type == DT_DIR && std::string(epdf->d_name) != ".." && std::string(epdf->d_name) != ".")
-                    : (epdf->d_type == DT_DIR && strstr(epdf->d_name, "..") == NULL &&
-                       strstr(epdf->d_name, ".") == NULL)) {
+                    : (epdf->d_type == DT_DIR && strstr(epdf->d_name, "..") == nullptr &&
+                       strstr(epdf->d_name, ".") == nullptr)) {
                 if (recursive) {
                     listFiles(path + epdf->d_name + "/", files, showHiddenDirs);
                 }
@@ -224,10 +228,10 @@ static inline void listFiles(
     closedir(dpdf);
 }
 
-static inline void listFiles(const std::string &path, std::function<void(const std::string &)> cb) {
+static inline void listFiles(const std::string &path, const std::function<void(const std::string &)> &cb) {
     if (auto dir = opendir(path.c_str())) {
         while (auto f = readdir(dir)) {
-            if (!f->d_name || f->d_name[0] == '.')
+            if (f->d_name[0] == '.')
                 continue;
             if (f->d_type == DT_DIR) {
                 listFiles(path + f->d_name + "/", cb);
