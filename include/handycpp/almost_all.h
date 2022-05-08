0aace23ed1104ea5250551057f56e2f0b2f0dcbe
zhangfuwen <zhangfuwen@bytedance.com>
2022-05-08 13:25:36 +0800

    add all.h to include almost all headers

---
+++ include/handycpp/almost_all.h
@@ -0,0 +1,19 @@
+//
+// Created by zhangfuwen on 2022/5/8.
+//
+
+#ifndef HANDYCPP_ALL_H
+#define HANDYCPP_ALL_H
+
+#include "handycpp/logging.h"
+#include "handycpp/archive.h"
+#include "handycpp/dyntype.h"
+#include "handycpp/exec.h"
+#include "handycpp/file.h"
+#include "handycpp/flags.h"
+#include "handycpp/image.h"
+#include "handycpp/string.h"
+#include "handycpp/syntax.h"
+#include "handycpp/time.h"
+
+#endif // HANDYCPP_ALL_H
